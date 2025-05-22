d97d0426030f44610406e42f7fb8414c57fe3d58
devAmaresh <amprasad.2003@gmail.com>
2025-05-23 01:19:20 +0530

    fix build

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,6 @@
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { NextAuthOptions } from "next-auth";
-
-import { Session } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
